b00fe17005e67fd47c3192534c41befe8694837c
Sylvia Xie <100256628+SYLVIAXIE1997@users.noreply.github.com>
2022-11-18 17:57:24 +1100

    fix: fix testimonials

--- backend_sanity/schemas/testimonials.js
+++ backend_sanity/schemas/testimonials.js
@@ -1,30 +1,41 @@
 export default {
-  name: "testimonials",
-  title: "Testimonials",
-  type: "document",
-  fields: [
-    {
-      name: "name",
-      title: "Name",
-      type: "string",
-    },
-    {
-      name: "company",
-      title: "Company",
-      type: "string",
-    },
-    {
-      name: "imageurl",
-      title: "Imageurl",
-      type: "image",
-      options: {
-        hotspot: true,
-      },
-    },
-    {
-      name: "feedback",
-      title: "Feedback",
-      type: "string",
-    },
-  ],
+    name: "testimonials",
+    title: "Testimonials",
+    type: "document",
+    fields: [
+        {
+            name: "name",
+            title: "Name",
+            type: "string",
+        },
+        {
+            name: "company",
+            title: "Company",
+            type: "string",
+        },
+        {
+            name: "imageurl",
+            title: "Imageurl",
+            type: "image",
+            imgurl: 'imgUrl',
+            options: {
+                hotspot: true,
+            },
+        },
+        // {
+        // //     name: 'imgUrl',
+        // //     title: 'ImgUrl',
+        // //     type: 'image',
+        // //     imgurl: 'imgUrl',
+        // //     options: {
+        // //         hotspot: true,
+        // //     },
+        // // },
+
+        {
+            name: "feedback",
+            title: "Feedback",
+            type: "string",
+        },
+    ],
 };
