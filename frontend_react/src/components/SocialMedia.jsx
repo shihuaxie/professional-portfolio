75231997fd26a4fb9a7b5e5b5acaa0030ac5c80f
Sylvia Xie <100256628+SYLVIAXIE1997@users.noreply.github.com>
2022-11-24 17:53:33 +1100

    chore: linked my socialmedia

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,19 +1,23 @@
 // snippet 'rsc'
 import React from 'react';
-import {BsTwitter, BsInstagram} from "react-icons/bs";
+import {BsLinkedin, BsGithub} from "react-icons/bs";
 import {FaFacebook} from "react-icons/fa";
 
 const SocialMedia = () => {
     return (
         <div className='app__social'>
             <div>
-                <BsTwitter/>
+                <a href='https://www.linkedin.com/in/shihua-xie-136282197' target='_blank' rel='noreferrer'>
+                    <BsLinkedin/>
+                </a>
+            </div>
+            <div>
+                <a href='https://github.com/shihuaxie' target='_blank' rel='noreferrer'>
+                    <BsGithub/>
+                </a>
             </div>
             <div>
                 <FaFacebook/>
-            </div>
-            <div>
-                <BsInstagram/>
             </div>
         </div>
     );
