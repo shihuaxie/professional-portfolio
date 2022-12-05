0df76edf90fabaf265a7beec1fd87966e05ec595
Sylvia Xie <100256628+SYLVIAXIE1997@users.noreply.github.com>
2022-12-05 15:02:29 +1100

    fix: fix a issue in skills

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -100,6 +100,6 @@ const Skills = () => {
 // export default AppWrap(Skills, 'skills');
 export default AppWrap(
     MotionWrap(Skills, 'app__skills'),
-    'skill',
+    'skills',
     'app__whitebg',
 );
