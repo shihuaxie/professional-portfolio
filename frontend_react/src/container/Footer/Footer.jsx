6ada3de5d2a1044081546dd07ce6f248dc1352a7
Sylvia Xie <100256628+SYLVIAXIE1997@users.noreply.github.com>
2023-01-12 13:41:22 +1100

    fix: fix works page

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -42,7 +42,7 @@ const Footer = () => {
             <div className="app__footer-cards">
                 <div className="app__footer-card ">
                     <img src={images.email} alt="email" />
-                    <a href="mailto:sylvia@gmail.com" className="p-text">sylvia@gmail.com</a>
+                    <a href="mailto:sylvia501628@gmail.com" className="p-text">sylvia501628@gmail.com</a>
                 </div>
                 <div className="app__footer-card">
                     <img src={images.mobile} alt="phone" />
